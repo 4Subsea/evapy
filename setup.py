96c5d2ae11fb1173d6033cbdd03c819003511d7c
Ali Cetin <ace@4subsea.com>
2016-06-05 20:29:18 +0200

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(name='evapy',
+      version='0.1.0.dev0',
+      description='Extreme value analysis',
+      author='4Subsea',
+      author_email='ace@4subsea.com',
+      url='',
+      keywords='extreme value statistics',
+      license='MIT',
+      packages=[
+          'evapy'
+      ],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Other Audience',
+          'Topic :: Scientific/Engineering',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT License'
+      ],
+      install_requires=[],
+      zip_safe=False)
