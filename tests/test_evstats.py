e564e17a0b764a568983acde770dd086cc4b061b
Ali Cetin <ace@4subsea.com>
2016-06-07 22:31:12 +0200

    test: add test for argupcross

--- tests/test_evstats.py
+++ tests/test_evstats.py
@@ -54,9 +54,9 @@ class Test__argupcross(unittest.TestCase):
         np.testing.assert_array_equal(calculated, expected)
 
     def test_simple_find_none(self):
-        x = np.array([0., 1., 2., 3., 4., 5.])
+        x = np.array([0.1, 1., 2., 3., 4., 5.])
         calculated = evstats._argupcross(x, x_up=0.)
-        expected = np.array([True, False, False, False, False, False])
+        expected = np.array([False, False, False, False, False, False])
         np.testing.assert_array_equal(calculated, expected)
 
 
@@ -77,4 +77,36 @@ class Test_argrelmax(unittest.TestCase):
         x = np.array([0., 1., 2., 3., 4., 5.])
         calculated = evstats.argrelmax(x)
         expected = np.array([5])
+        np.testing.assert_array_equal(calculated, expected)
+
+
+class Test_argupcross(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_simple_find(self):
+        x = np.array([0., 1., -1., -2., -1., 1., 0.])
+        calculated = evstats.argupcross(x, x_up=0.)
+        expected = np.array([0., 4])
+        np.testing.assert_array_equal(calculated, expected)
+
+    def test_simple_find_w_zero(self):
+        x = np.array([0., 1., 0., -1., -2., -1., 0., 1., 0.])
+        calculated = evstats.argupcross(x, x_up=0.)
+        expected = np.array([0, 6])
+        np.testing.assert_array_equal(calculated, expected)
+
+    def test_simple_find_w_other(self):
+        x = np.array([0., 1., 0., -1., -2., -1., 0., 1., 0.]) + 2.
+        calculated = evstats.argupcross(x, x_up=2.)
+        expected = np.array([0., 6.])
+        np.testing.assert_array_equal(calculated, expected)
+
+    def test_simple_find_none(self):
+        x = np.array([0.1, 1., 2., 3., 4., 5.])
+        calculated = evstats.argupcross(x, x_up=0.)
+        expected = np.array([0.])
         np.testing.assert_array_equal(calculated, expected)
