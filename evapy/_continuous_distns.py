f8c85f4fc4aa65b71655544fa2fd0f3b30577c4d
Ali Cetin <ace@4subsea.com>
2016-06-20 06:19:47 +0900

    feat: overwrite fit for exp tail distributions

--- evapy/_continuous_distns.py
+++ evapy/_continuous_distns.py
@@ -14,8 +14,9 @@ from __future__ import division
 
 from scipy import special
 from scipy.stats import rv_continuous
+from scipy import optimize
 
-from numpy import (exp, log, sqrt, pi)
+from numpy import (exp, log, sqrt, pi, inf)
 
 import numpy as np
 
@@ -181,4 +182,94 @@ class gen_exp_tail_gen(rv_continuous):
 
     def _sf(self, x, c, q):
         return np.exp(-x**c + np.log(q))
-genexpt = gen_exp_tail_gen(name='genexpt')
+
+    def _penalized_nnlf(self, theta, x):
+        '''
+        Return special purspose lsq objective error function to minimize.
+        Method is overwritten to hook into scipy.stats optimization framework.
+
+            '''
+        try:
+            loc = theta[-2]
+            scale = theta[-1]
+            args = tuple(theta[:-2])
+        except IndexError:
+            raise ValueError("Not enough input arguments.")
+
+        if not self._argcheck(*args) or scale <= 0:
+            return inf
+
+        x = np.asarray((x-loc) / scale)
+        x.sort()
+
+        if np.isneginf(self.a).all() and np.isinf(self.b).all():
+            Nbad = 0
+        else:
+            cond0 = (x <= self.a) | (self.b <= x)
+            Nbad = sum(cond0)
+            if Nbad > 0:
+                x = x[~cond0]
+
+        N = len(x)
+        f_ecdf = np.array([(i + 1 - 0.3)/(N + 0.4) for i in range(N)])
+        error = np.abs(log(1/(1 - f_ecdf)) - 
+                       log(1/(1 - self._cdf(x, *args))))**2.
+        return np.sum(error) + Nbad * 10000.
+genexpt = gen_exp_tail_gen(name='genexpt', a=0.)
+
+
+class acer_o1_gen(rv_continuous):
+    """
+    ACER (1st order) extreme value distribution.
+    """
+    def _pdf(self, x, c, q):
+        return c*x**(c-1.)*exp(-x**c + log(q))
+
+    def _logpdf(self, x, c, q):
+        return log(c) + (c-1)*log(x) - x**c + log(q)
+
+    def _cdf(self, x, c, q):
+        return -special.expm1(-x**c + log(q))
+
+    def _logcdf(self, x, c, q):
+        return special.log1p(-np.exp(-x**c + log(q)))
+
+    def _ppf(self, f, c, q):
+        return (np.log(q) - special.log1p(-f))**(1./c)
+
+    def _sf(self, x, c, q):
+        return np.exp(-x**c + np.log(q))
+
+    def _penalized_nnlf(self, theta, x):
+        '''
+        Return special purspose lsq objective error function to minimize.
+        Method is overwritten to hook into scipy.stats optimization framework.
+
+            '''
+        try:
+            loc = theta[-2]
+            scale = theta[-1]
+            args = tuple(theta[:-2])
+        except IndexError:
+            raise ValueError("Not enough input arguments.")
+
+        if not self._argcheck(*args) or scale <= 0:
+            return inf
+
+        x = np.asarray((x-loc) / scale)
+        x.sort()
+
+        if np.isneginf(self.a).all() and np.isinf(self.b).all():
+            Nbad = 0
+        else:
+            cond0 = (x <= self.a) | (self.b <= x)
+            Nbad = sum(cond0)
+            if Nbad > 0:
+                x = x[~cond0]
+
+        N = len(x)
+        f_ecdf = np.array([(i + 1 - 0.3)/(N + 0.4) for i in range(N)])
+        error = np.abs(log(1/(1 - f_ecdf)) - 
+                       log(1/(1 - self._cdf(x, *args))))**2.
+        return np.sum(error) + Nbad * 10000.
+genexpt = gen_exp_tail_gen(name='genexpt', a=0.)
