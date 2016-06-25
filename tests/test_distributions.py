fb3b79fc6aea6b5f57c82973242420eb47633458
Ali Cetin <ace@4subsea.com>
2016-06-25 14:32:37 +0900

    test: add tests for gumbel

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -62,7 +62,40 @@ class Test_frechet_r_gen(unittest.TestCase):
         expected = self.dist.cdf(2.5, 2., loc=0.5, scale=2.)
         self.assertAlmostEqual(calculated, expected, places=4)
 
-    def test_instance_weibull_max(self):
+    def test_instance_weibull_min(self):
         calculated = dist.weibull_min.cdf(2.5, 2., loc=0.5, scale=2.)
         expected = self.dist.cdf(2.5, 2., loc=0.5, scale=2.)
+        self.assertAlmostEqual(calculated, expected, places=4)
+
+
+class Test_gumbel_r_gen(unittest.TestCase):
+    def setUp(self):
+        self.dist = dist._distns.gumbel_r_gen()
+
+    def tearDown(self):
+        pass
+
+    def test_cdf(self):
+        calculated = self.dist.cdf(3., loc=1., scale=2.)
+        expected = np.exp(-np.exp(-1.))
+        self.assertAlmostEqual(calculated, expected, places=4)
+
+    def test_pdf(self):
+        calculated = self.dist.pdf(3., loc=1., scale=2.)
+        expected = np.exp(-1. - np.exp(-1.))/2.
+        self.assertAlmostEqual(calculated, expected, places=4)
+
+    def test_ppf(self):
+        calculated = self.dist.ppf(0.5, loc=1., scale=2.)
+        expected = 1. - 2.*np.log(np.log(2))
+        self.assertAlmostEqual(calculated, expected, places=4)
+
+    def test_instance_gumbel(self):
+        calculated = dist.gumbel.cdf(2.5, loc=0.5, scale=2.)
+        expected = self.dist.cdf(2.5, loc=0.5, scale=2.)
+        self.assertAlmostEqual(calculated, expected, places=4)
+
+    def test_instance_gumbel_max(self):
+        calculated = dist.gumbel_max.cdf(2.5, loc=0.5, scale=2.)
+        expected = self.dist.cdf(2.5, loc=0.5, scale=2.)
         self.assertAlmostEqual(calculated, expected, places=4)
