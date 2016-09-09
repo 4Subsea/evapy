7b981ed36126f0f0fa049c300fb7c2cd97a11911
Ali Cetin <ace@4subsea.com>
2016-09-10 00:27:49 +0200

    feat: bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='evapy',
-      version='0.1.0b0',
+      version='0.1.0',
       description='Extreme value analysis',
       author='4Subsea',
       author_email='ace@4subsea.com',
