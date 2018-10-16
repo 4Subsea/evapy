5edba3aff48382c01baaebc8294cadd757925b0c
Ali Cetin <ace@4subsea.com>
2018-10-16 21:40:09 +0200

    version: bump version to 0.2.0 and update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='evapy',
-      version='0.1.2',
+      version='0.2.0',
       license='MIT',
       description='Extreme value analysis of time series',
       keywords='extreme value statistics',
@@ -17,8 +17,10 @@ setup(name='evapy',
           'Development Status :: 4 - Beta',
           'Intended Audience :: Other Audience',
           'Topic :: Scientific/Engineering',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'License :: OSI Approved :: MIT License'
       ],
       install_requires=[
